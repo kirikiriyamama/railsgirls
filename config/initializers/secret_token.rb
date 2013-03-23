9d1e76fe532eed8f38b0ce7cc2673c2deabfe7d8
remain <ponde.ponde.pond@gmail.com>
2013-03-23 16:10:25 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '6a78fc4135a0cec033ef80bcbe5388a1b97bb81e45c3c14d835ede2d8be3f5956e7d88035910ec74dd6a25d572493ef274fc572f024732225d6019a2ece3b5be'
